d74c9504fd95ed37c84198b83fc21f72116589e9
EC2 Default User <ec2-user@ip-172-31-13-47.ap-northeast-1.compute.internal>
2020-07-13 14:22:56 +0000

    tweetsコントローラー、リダイレクト設定(indexアクション以外)

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,6 @@
 class TweetsController < ApplicationController
+  before_action :move_to_index, except: :index
+  
   def index
     @tweets = Tweet.all
   end
@@ -13,5 +15,11 @@ class TweetsController < ApplicationController
   private
   def tweet_params
     params.permit(:name, :text, :image)
+  end
+  
+  def move_to_index
+    unless user_signed_in?
+      redirect_to :root
+    end
   end
 end
