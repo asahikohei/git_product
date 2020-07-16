cc9af67f94e3519f985924f985fa1559ed3aec41
EC2 Default User <ec2-user@ip-172-31-13-47.ap-northeast-1.compute.internal>
2020-07-16 13:53:48 +0000

    tweets/editアクションへのルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'tweets#index'
-  resources :tweets, only: [:new, :create, :destroy]
+  resources :tweets, only: [:new, :create, :destroy, :edit]
   resources :users, only: [:show]
 end
