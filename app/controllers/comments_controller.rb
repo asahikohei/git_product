e881f8ed8622f46fa05ddca061ab40a4399ad4fd
EC2 Default User <ec2-user@ip-172-31-13-47.ap-northeast-1.compute.internal>
2020-07-17 04:31:05 +0000

    commentsコントローラー/createアクションの設定

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,10 @@
 class CommentsController < ApplicationController
+  def create
+    Comment.create(comment_params)
+  end
+  
+  private
+  def comment_params
+    params.permit(:text, :tweet_id).merge(user_id: current_user.id)
+  end
 end
