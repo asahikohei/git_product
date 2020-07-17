034101424e47f9817b234bf00eb1c3f470d5530e
EC2 Default User <ec2-user@ip-172-31-13-47.ap-northeast-1.compute.internal>
2020-07-17 05:06:18 +0000

    tweets/showビューにコメント一覧表示作成、諸々デザインの変更

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,11 @@
 class CommentsController < ApplicationController
   def create
     Comment.create(comment_params)
+    redirect_to tweet_path
   end
   
   private
   def comment_params
-    @tweet = Tweet.find(params[:id])
-    params.permit(:text).merge(user_id: current_user.id, tweet_id: @tweet.id)
+    params.permit(:text, :tweet_id).merge(user_id: current_user.id)
   end
 end
